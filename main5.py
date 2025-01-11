29a9140b7fee536cb58f624c0880d76fb7059402
Ajetunmobi Anjolaoluwa <abdfashion.2014@gmail.com>
2025-01-11 22:19:05 +0100

    project

--- main5.py
+++ main5.py
@@ -2,8 +2,6 @@ attendance = int(input("Enter the number of classes attended: "))
 project = int(input("Enter the number of project done: "))
 min_project = 70
 min_attendance = 75
-
-    # Check if the user meets the age and score criteria
 if project >= min_project and attendance >= min_attendance:
     print("You are eligible for the exam")
 else:
